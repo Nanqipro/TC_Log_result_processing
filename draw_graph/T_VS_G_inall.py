f08fd554b48bcae051e52b3e9797460322494927
nanqipro <2280285631@qq.com>
2024-11-24 23:14:03 +0800

    20241124

--- draw_graph/T_VS_G_inall.py
+++ draw_graph/T_VS_G_inall.py
@@ -1,26 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Load the Excel file into a DataFrame
-file_path = './TrustVSGroupTC/lowDegree_log_copy.xlsx'
-data = pd.read_excel(file_path)
+# 读取Excel文件
+file_path = './TrustVSGroupTC/lowDegree-buildIndex.xlsx'  # 修改为实际的文件路径
+df = pd.read_excel(file_path)
 
-# Assuming the columns are named 'dataset_Vnumber' and 'speed-up'
-# Plotting the scatter plot
+# 检查数据结构，确保列名正确
+print(df.head())
+
+# 假设 Excel 中列名分别是 'edge-count' 和 'speed-up'，如果不同请根据实际修改
+x = df['edge-count']
+y = df['speed-up']
+
+# 对数据进行对数变换，以e为底的对数
+x_log = np.log1p(x)
+# y_log = np.log1p(y)
+
+# 创建散点图
 plt.figure(figsize=(10, 6))
-plt.scatter(data['dataset_Vnumber'], data['speed-up'], color='r', alpha=0.6, edgecolors='w', linewidth=0.5)
-plt.xlabel('Dataset Vnumber')
-plt.ylabel('Speed-up (GroupTC-Hash vs Trust)')
-# plt.title('Speed-up of GroupTC-Hash Algorithm Compared to Trust Algorithm')
+plt.scatter(x_log, y, marker='o', color='blue', alpha=0.5)
+
+# 设置横纵坐标的刻度
+plt.xticks(np.arange(10, int(x_log.max()) + 1, 0.5))
+plt.yticks(np.arange(0, int(y.max())+0.5, 0.2))  # 每隔 0.5 进行刻度显示
+
+# 设置标题和标签
+# plt.title('Log Transformed Edge Count vs Speed-up', fontsize=14)
+plt.xlabel('Log(Edge Count)', fontsize=12)
+plt.ylabel('Log(Speed-up)', fontsize=12)
+
+# 显示网格
 plt.grid(False)
 
-# Adjusting the scale of axes
-plt.xticks(rotation=45)
-plt.tight_layout()
-
-# Save the plot to a file
-plot_path = './TrustVSGroupTC/T_VS_G_inall.png'
-plt.savefig(plot_path)
-
-# Display the plot
+# 显示图像
 plt.show()
+
--- draw_graph/preprocessing.py
+++ draw_graph/preprocessing.py
@@ -2,16 +2,16 @@
 import pandas as pd
 
 # Load the data from the provided text file
-file_path = './TrustVSGroupTC/GroupTCHash_lowDegree_log.txt'
+file_path = './TrustVSGroupTC/low100Vcount.txt'
 
 # Read the text file into a DataFrame using sep='\s+'
 data = pd.read_csv(file_path, sep='\s+', header=None)
 
 # Assign column names for better understanding
-data.columns = ['Column1', 'Column2']
+data.columns = ['Column1', 'Column2', 'Column3', 'Column4', 'Column5', 'Column6', 'warpfirstvertex', 'warpfirstedge', 'nocomputefirstvertex', 'nocomputefirstedge']
 
 # Save the DataFrame to an Excel file
-excel_file_path = './TrustVSGroupTC/lowDegree.xlsx'
+excel_file_path = './TrustVSGroupTC/low100Vcount.xlsx'
 data.to_excel(excel_file_path, index=False)
 
 print(f"Excel file saved to {excel_file_path}")
